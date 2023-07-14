a5f0c46c926d825047dac456cb2dc5af2b9ab1b9
Ryohei273 <52483529+Ryohei273@users.noreply.github.com>
2023-07-14 14:24:08 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-cb48.up.railway.app']
+CSRF_COOKIE_SECURE = True
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
